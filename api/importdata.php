8df5f4c73cf0b17e500251e68f06c88eb705ba12
woraphat <164898787+woraphatLaboratory@users.noreply.github.com>
2024-04-22 22:52:54 +0700

    .

--- api/importdata.php
+++
@@ -1,8 +0,0 @@
-<?php
-include ('../connect.php') ;
-
-for($i=1;$i<400;$i++){
-    $sql = "INSERT INTO data_ecg_6_lead SELECT * FROM data_test WHERE time_id = ".$i;
-    $result = $conn->query($sql);
-    sleep(0.25);
-}
